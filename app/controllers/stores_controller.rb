d3690959e859e3d2bbe7428e7946fab2f5794a4e
Myles Woerner <myleswoerner@gmail.com>
2016-11-08 20:26:49 -0600

    Add pages for individual products

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,20 @@
 class StoresController < ApplicationController
-  def single_product_method
+  def first_product_method
+    @product = Store.first
+    render 'atomic_vantage.html.erb'
+  end
+
+  def second_product_method
+    @product = Store.second
+    render 'soulseven.html.erb'
+  end
+
+  def third_product_method
+    @product = Store.third
+    render 'lineSFB.html.erb'
+  end
+  def all_product_method
+    @products = Store.all
     render 'homepage.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/single_product' => 'stores#single_product_method'
+  get '/homepage' => 'stores#all_product_method'
+  get '/atomic_vantage' => 'stores#first_product_method'
+  get '/soulseven' => 'stores#second_product_method'
+  get '/lineSFB' => 'stores#third_product_method'
 end
